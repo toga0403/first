1700acc2ae5a35a434af1cfd13c8ccc43868153d
toga0403.git <gakuwari43@icloud.com>
2023-06-25 19:48:41 +0900

    paginate commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     //use宣言は外部にあるクラスをPostController内にインポートできる。
     //この場合、App\Models内のPostクラスをインポートしている。
-    use App\Models\Post;
     /**
      * Post一覧を表示する
      * 
@@ -17,6 +17,6 @@ class PostController extends Controller
      */
     public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
     {
-        return $post->get();//$postの中身を戻り値にする。
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
     }
 }
